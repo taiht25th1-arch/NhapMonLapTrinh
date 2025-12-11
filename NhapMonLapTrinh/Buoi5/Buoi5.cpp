42f5fc1d234e61202838c6c1f442e26dc0830cc3
Administrator <Administrator@PMT2035>
2025-12-11 14:24:16 +0700

    Buoi 5 va 6

---
+++ NhapMonLapTrinh/Buoi5/Buoi5.cpp
@@ -0,0 +1,45 @@
++
+
+#include <iostream>
+using namespace std;
+void cau1() {
+    int a, b, c;
+    cout << "Nhap a, b, c: ";
+    cin >> a >> b >> c;
+    if (a > b && a > c)
+        cout << a;
+    if (b > a && b > c)
+        cout << b;
+    if (c > a && c > b)
+        cout << c;
+}
+void cau2() {
+    float a, b;
+    cout << "Nhap a, b: ";
+    cin >> a >> b;
+    if (a * b > 0)
+        cout << "cung dau";
+    else
+        cout << "khac dau";
+}
+void cau3() {
+    int a, b;
+    cout << "Nhap a, b: ";
+    cin >> a >> b;
+    if (a == 0) {
+        if (b == 0)
+            cout << "Phuong trinh vo so nghiem" << endl;
+        else
+            cout << "Phuong trinh vo nghiem" << endl;
+    }
+    else {
+        cout << "Nghiem la: " << (-b * 1.0 / a) << endl;
+    }
+}
+
+int main()
+{
+    cau3();
+}
+
+
---
+++ NhapMonLapTrinh/Buoi6/Buoi6.cpp
@@ -0,0 +1,38 @@
+
+#include <iostream>
+using namespace std;
+
+void cau1() {
+    int n, tong = 0;
+    cout << "Nhap n: ";
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        tong += i;
+    }
+    cout << "Tong: " << tong << endl;
+}
+void cau2() {
+    int n, tich = 1;
+    cout << "Nhap n: ";
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        tich *= i;
+    }
+    cout << "Tich: " << tich << endl;
+}
+void cau3() {
+    int x, n, tich = 1;
+    cout << "Nhap x, n: ";
+    cin >> x >> n;
+    for (int i = 1; i <= n; i++) {
+        tich *= x;
+    }
+    cout << "Tich: " << tich << endl;
+}
+int main()
+{
+    cau3();
+}
+//sdjflsdk
+
+
